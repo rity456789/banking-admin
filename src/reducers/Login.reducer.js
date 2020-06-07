6654725e05f413b600771fcc71599eb6c7c5810b
1612041_phbinh <1612041_phbinh>
2020-06-07 16:02:39 +0700

    Init login UI

---
+++ src/reducers/Login.reducer.js
@@ -0,0 +1,8 @@
+const LoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default LoginReducer;
---
+++ src/reducers/reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+import LoginReducer from "./Login.reducer";
+
+const reducer = combineReducers({
+  LoginReducer,
+});
+
+export default reducer;
